7d1ecaf9525b9cc5413bd4098e1a9b57f25a1166
Miloslav Slizik <milo.slizik@gmail.com>
2017-11-24 18:18:23 +0100

    hotova implementacia dijkstru zo vsetkych uzlov

--- prototyp/host/ABW.py
+++ prototyp/host/ABW.py
@@ -6,6 +6,33 @@ import time
 import subprocess
 from collections import namedtuple
 from timeit import default_timer
+from collections import defaultdict
+from heapq import heappush, heappop
+
+def Dijkstra(graph, start):
+    A = {}
+    for i in graph:
+     A[i] = None
+    queue = [(0, start)]
+    while queue:
+        path_len, v = heappop(queue)
+        if A[v] is None: # v is unvisited
+            A[v] = path_len
+            for w, edge_len in graph[v].items():
+                if A[w] is None:
+                    if path_len == 0:
+                     heappush(queue, ( edge_len, w))
+                    elif path_len > edge_len:
+                     heappush(queue, ( path_len, w))
+                    else:
+                     heappush(queue, ( edge_len, w))
+    for i in A:
+     if A[i] !=  None:
+      print "DIJKSTRA Z  " + start +" DO "+ i + " MAX ABW :  " + str(A[i] * -1)
+     else:
+      print "DIJKSTRA Z  " + start +" DO "+ i + " MAX ABW :  " + str(A[i])
+    print "\n"
+    return [0 if x is None else x for x in A]
 
 if len(sys.argv) <= 1:
  print "Pre spustenie programu je potrebne zadat argument: ako casto ma program vypisovat dostupny bandwidth v sekundach"
@@ -22,11 +49,13 @@ MyStruct2 = namedtuple("MyStruct2",["linkBW","src_switch","dst_switch","src_port
 MyStruct3 = namedtuple("MyStruct3",["linkBW","src_switch","dst_switch","src_port","dst_port","availableBW"])
 MyStruct4 = namedtuple("MyStruct4",["sender","reciever"])
 LinkArray = []
-time.sleep(10)
+# Change Routing policy in floodlight
+os.system("curl -s -X POST -d '{ \"metric\" : \"hopcount\" }' http://localhost:8080/wm/routing/metric/json > /dev/null")
 # Generate info about links in topology
-os.system("curl -X GET http://"+address+":8080/wm/topology/links/json | python -m json.tool > links.json")
+os.system("curl -s -X GET http://"+address+":8080/wm/topology/links/json | python -m json.tool > links.json")
 #enable statistics in floodlight
-os.system("curl -X POST http://"+address+":8080/wm/statistics/config/enable/json")
+os.system("curl -s -X POST http://"+address+":8080/wm/statistics/config/enable/json > /dev/null")
+time.sleep(10)
 # sem je potrebne vpisat vsetky kapacity linkov urcene v mininete
 link1 = MyStruct(linkBW=10,src_switch="00:00:00:00:00:00:00:01",dst_switch="00:00:00:00:00:00:00:02")
 link2 = MyStruct(linkBW=20,src_switch="00:00:00:00:00:00:00:02",dst_switch="00:00:00:00:00:00:00:03")
@@ -35,8 +64,8 @@ link4 = MyStruct(linkBW=10,src_switch="00:00:00:00:00:00:00:02",dst_switch="00:0
 link5 = MyStruct(linkBW=5,src_switch="00:00:00:00:00:00:00:01",dst_switch="00:00:00:00:00:00:00:04")
 ## assignement of link capacity to switches
 with open('links.json') as json_links:
-    a = json.load(json_links)
-    for value1 in a:
+   a = json.load(json_links)
+   for value1 in a:
       if value1['src-switch'] == link1[1] and value1['dst-switch'] == link1[2]:
         LinkArray.append(MyStruct2(linkBW=link1[0],src_switch=value1['src-switch'],dst_switch=value1['dst-switch'],src_port=value1['src-port'],dst_port=value1['dst-port']))
       elif value1['src-switch'] == link2[1] and value1['dst-switch'] == link2[2]:
@@ -53,15 +82,31 @@ try:
 ## calculate avalilible bandwith for every link DONE
     FinalArray = []
     for item in LinkArray:
-           dstBWrx = subprocess.check_output('curl -X GET http://'+address+':8080/wm/statistics/bandwidth/' + item.dst_switch + '/' + str(item.dst_port) + '/json | python -m json.tool | grep rx | tr -dc \'0-9\'', shell=True)
-           dstBWtx = subprocess.check_output('curl -X GET http://'+address+':8080/wm/statistics/bandwidth/' + item.dst_switch + '/' + str(item.dst_port) + '/json | python -m json.tool | grep tx | tr -dc \'0-9\'', shell=True)
-           print item.dst_switch +';'+ str(item.dst_port) + 'BWrx:  ' + str(dstBWrx) + '  BWtx: ' + str(dstBWtx)
+           dstBWrx = subprocess.check_output('curl -s -X GET http://'+address+':8080/wm/statistics/bandwidth/' + item.dst_switch + '/' + str(item.dst_port) + '/json | python -m json.tool | grep rx | tr -dc \'0-9\'', shell=True)
+           dstBWtx = subprocess.check_output('curl -s -X GET http://'+address+':8080/wm/statistics/bandwidth/' + item.dst_switch + '/' + str(item.dst_port) + '/json | python -m json.tool | grep tx | tr -dc \'0-9\'', shell=True)
+           print ' Namerane hodnoty RX a TX bits-per-second na switchi : '+item.dst_switch +' porte: '+ str(item.dst_port) + '  rx:  ' + str(dstBWrx) + '  tx: ' + str(dstBWtx)
            dstBW = (int(dstBWrx)  + int(dstBWtx))
            FinalArray.append(MyStruct3(linkBW=item.linkBW,src_switch=item.src_switch,dst_switch=item.dst_switch,src_port=item.src_port,dst_port=item.dst_port,availableBW=(item.linkBW*1000000)-dstBW))
-           print 'abc   ' + str(len(FinalArray))
-           print ' BANDWIDTH cesta : ' + item.src_switch + 'dst  ' + item.dst_switch + '  SRC PORT : ' + str(item.src_port) + ' DST PORT : ' + str(item.dst_port) + '  '   + str(item.linkBW*1000000-dstBW)
+           print ' Available BW na linku : ' + item.src_switch + 'dst  ' + item.dst_switch + '  SRC PORT : ' + str(item.src_port) + ' DST PORT : ' + str(item.dst_port) + ' =  '   + str(item.linkBW*1000000-dstBW)
+           print "\n"
+#DIJKST
+    vertices = []
+    for i in FinalArray:
+     vertices.append(i.src_switch)
+     vertices.append(i.dst_switch)
+    vertices = list(set(vertices))
+    AdjacentVertices = defaultdict(dict)
+    for i in vertices:
+     for j in FinalArray:
+      if i == j.src_switch:
+       AdjacentVertices[i][j.dst_switch] = j.availableBW * -1
+      elif i == j.dst_switch:
+       AdjacentVertices[i][j.src_switch] = j.availableBW * -1
+    #print AdjacentVertices
+    for i in AdjacentVertices:
+     Dijkstra(AdjacentVertices,i)
 #check attachement points of all hosts DONE
-    os.system("curl -X GET http://"+address+":8080/wm/device/ | python -m json.tool > devices.json")
+    os.system("curl -s -X GET http://"+address+":8080/wm/device/ | python -m json.tool > devices.json")
     DeviceArray = []
     HostStruct = namedtuple("HostStruct",["mac","switch","port"])
     with open('devices.json') as json_devices:
@@ -72,7 +117,7 @@ try:
        for key2 in key1["attachmentPoint"]:
          DeviceArray.append(HostStruct(mac=mac,switch=key2["switch"],port=key2["port"]))
 ##check all active flows besides controller generated ones -- and return distinct flows in network -DONE
-    os.system("curl -X GET http://"+address+":8080/wm/core/switch/all/flow/json | python -m json.tool > flows.json")
+    os.system("curl -s -X GET http://"+address+":8080/wm/core/switch/all/flow/json | python -m json.tool > flows.json")
     flows = []
     with open('flows.json') as json_flows:
      a = json.load(json_flows)
@@ -86,7 +131,6 @@ try:
      FlowsArray = []
      for key in distinctFlowsList:
       FlowsArray.append(MyStruct4(sender=key[0],reciever=key[1]))
-
 ## map hosts generating flows onto attachment point and get route betweeen hosts DONE
      os.system("rm route.json")
      SwitchAbwArray = []
@@ -101,7 +145,7 @@ try:
       SwitchAbwArray.append(SwitchToSwitchABW(src_switch=src_dpid,dst_switch=dst_dpid,availableBW=0))
 # create path from links -DONE
 # need to Remove file route.json at the end of every cycle
-      os.system("curl -X GET http://"+address+":8080/wm/routing/path/"+src_dpid+"/"+src_port+"/"+dst_dpid+"/"+dst_port+"/json | python -m json.tool > route.json")
+      os.system("curl -s -X GET http://"+address+":8080/wm/routing/path/"+src_dpid+"/"+src_port+"/"+dst_dpid+"/"+dst_port+"/json | python -m json.tool > route.json")
       with open("route.json") as f:
        a = json.load(f)
        for key in a:
@@ -117,10 +161,8 @@ try:
         minBW = 99999999999
         for i in SwitchArray:
          for j in FinalArray:
-#          print "I src SWITCH: " + i.src_switch + "  j src switch  "+ j.src_switch +" I DST SWITCH  "+i.dst_switch+"  j DST SWITCH "+j.dst_switch+" AVAILIABLE BW :" + str(j.availableBW) + " MIN BW " + str(minBW)
-          if ((i.src_switch == j.src_switch and i.dst_switch == j.dst_switch) and j.availableBW < minBW) or ((i.src_switch == j.dst_switch and i.dst_switch == j.src_switch) and j.availableBW < minBW) :
+          if ((i.src_switch == j.src_switch and i.dst_switch == j.dst_switch) and j.availableBW < minBW) or ((i.src_switch == j.dst_switch and i.dst_switch == j.src_switch) and j.availableBW < minBW):
            minBW = j.availableBW
-        #   print "AVAILABLEBANDWITH: " + str(j.availableBW)
          print "switche pre ktore vypisujeme bandwidth: SRC: " + i.src_switch + " DST: " + i.dst_switch
         print "dostupny bandwidth pre cestu : "  + str(minBW)
 ## TODO nejaky rozumny output do fileu
@@ -133,9 +175,8 @@ try:
     time.sleep(float(sys.argv[1]))
 except KeyboardInterrupt:
 # remove temp files needed for bandwidth calculation
-  os.system("rm links.json")
-  os.system("rm flows.json")
-  os.system("rm route.json")
-  os.system("rm devices.json")
+  #os.system("rm links.json")
+  #os.system("rm flows.json")
+  #os.system("rm route.json")
+  #os.system("rm devices.json")
   print("program bol ukonceny")
-# pushovat flowy cez staticflowpusherAPI
